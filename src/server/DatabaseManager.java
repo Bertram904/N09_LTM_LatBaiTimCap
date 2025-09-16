93f688a6b3dfc2459c42bc40499820a4198513f3
ngotuananh <ngotuananh9922@gmail.com>
2025-09-16 15:55:27 +0700

    initial project

---
+++ src/server/DatabaseManager.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package server;
+
+import entity.Player;
+import java.io.IOException;
+import java.sql.*;
+
+
+/**
+ *
+ * @author ngotu
+ */
+public class DatabaseManager {
+
+    private static final String URL_JDBC = "jdbc:mysql://localhost:3306/latbai_db";
+    private static final String URL_USER = "root";
+    private static final String URL_PASS = "Anh2210anh";
+    
+    private Connection conn;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public DatabaseManager() throws SQLException {
+        conn = DriverManager.getConnection(URL_JDBC, URL_USER, URL_PASS);
+    }
+    
+    public static Player getPlayer() throws SQLException, IOException {
+        String sql = "SELECT * FROM tblPlayer WHERE USERNAME = 'a'";
+        Player player = new Player();
+        PreparedStatement stm = conn.prepareStatement(sql);
+        ResultSet rs = stm.executeQuery(sql);
+        if (rs.next()) {
+                player = new Player();
+                player.setUsername(rs.getString("username"));
+        }
+        player.setUsername(rs.getString("username"));
+        return player;
+    }
+    public static void main(String[] args) throws SQLException {
+        Player x = getPlayer();
+        System.out.println(x.getUsername());
+    }
+}

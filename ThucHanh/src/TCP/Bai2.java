c575900d24539c631431c1ff82e170e008835851
ngotuananh <ngotuananh9922@gmail.com>
2025-09-26 18:00:37 +0700

    check pull

--- ThucHanh/src/TCP/Bai2.java
+++ ThucHanh/src/TCP/Bai2.java
@@ -2,16 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package server;
+package TCP;
 import java.io.*;
-import java.sql.*;
-import entity.Message;
-import java.util.*;
-
+import java.net.*;
 /**
  *
  * @author ngotu
  */
-public class GameRoom {
-    
+public class Bai2 {
+    public static void main(String[] args) throws Exception{
+        Socket socket = new Socket("203.162.10.109", 2207);
+        DataIn
+    }
 }

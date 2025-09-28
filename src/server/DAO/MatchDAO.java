65ef1069b5a31583703ec20bd04eb94c61b5c1c6
ngotuananh <ngotuananh9922@gmail.com>
2025-09-29 00:36:40 +0700

    add DAO

---
+++ src/server/DAO/MatchDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package server.DAO;
+
+/**
+ *
+ * @author ngotu
+ */
+public class MatchDAO {
+    public static void main(String[] args) {
+        
+    }
+}
